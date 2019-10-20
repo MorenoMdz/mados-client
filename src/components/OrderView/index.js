fb0c8c7c282b90f767b6483daf4b13d892f6f7d6
Moreno Antunes <morenomdz@gmail.com>
2019-10-20 09:34:48 -0300

    Service Order details

--- src/components/OrderView/index.js
+++ src/components/OrderView/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -14,167 +14,184 @@ import {
 
 import ServiceOrdersActions from '../../store/ducks/serviceOrders';
 
-const OrderView = ({ orderId, expand, fetchServiceOrderRequest }) => {
+const OrderView = ({
+  orderId,
+  expand,
+  serviceOrder,
+  fetchServiceOrderRequest,
+}) => {
+  const [loading, setLoading] = useState(true);
+  const {
+    client,
+    creator,
+    diagStatus,
+    diagnostics,
+    equipment,
+    osStatus,
+    paymentStatus,
+    priority,
+    repairStatus,
+    repairs,
+    store,
+  } = serviceOrder;
   useEffect(() => {
-    if (orderId) fetchServiceOrderRequest(orderId);
+    if (orderId) fetchServiceOrderRequest(orderId, setLoading(false));
   }, []);
+  console.log('serviceOrder', serviceOrder);
   // TODO Text Field / TextArea field components
   return (
     <Container expand={expand}>
-      {orderId ? (
-        <>
-          {/* TODO Header Full grid block */}
-          <OrderHeader>Ordem de Serviço</OrderHeader>
-          <OrderBody>
-            <CardBody>
-              <h5>Cliente</h5>
-              <div>
-                <TextDiv>
-                  <small>Nome: </small>
-                  <strong>Tony</strong>
-                </TextDiv>
-                <TextDiv>
-                  <small>Telefone 1: </small>
-                  <strong>133 133 133 133</strong>
-                </TextDiv>
-              </div>
-              <div>
-                <TextDiv>
-                  <small>CPF: </small>
-                  <strong>133 133 133 133</strong>
-                </TextDiv>
-                <TextDiv>
-                  <small>Telefone 2: </small>
-                  <strong>133 133 133 133</strong>
-                </TextDiv>
-              </div>
-              <TextDiv>
-                <small>Email: </small>
-                <strong>tony@tony.com</strong>
-              </TextDiv>
-              <TextDiv>
-                <small>Endereço: </small>
-                <strong>aaaaaaaaaaa</strong>
-              </TextDiv>
-              <TextAreaDiv>
-                <small>Obs:</small>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Numquam laborum incidunt, beatae soluta fugit ratione. Sed et
-                  eius veritatis soluta, unde cupiditate nesciunt voluptas
-                  voluptatem. Molestiae optio nihil blanditiis quam?
-                </p>
-              </TextAreaDiv>
-            </CardBody>
-
-            <CardBody>
-              <h5>Equipamento</h5>
-              <div>
-                <TextDiv>
-                  <small>Aparelho: </small>
-                  <strong>Ps4</strong>
-                </TextDiv>
-                <TextDiv>
-                  <small>Modelo 1: </small>
-                  <strong>Pro</strong>
-                </TextDiv>
-              </div>
-              <div>
-                <TextDiv>
-                  <small>Serial: </small>
-                  <strong>aaaaaaaaaaa</strong>
-                </TextDiv>
-              </div>
-              <TextDiv>
-                <small>Acessórios: </small>
-                <strong>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Aliquam, sequi.
-                </strong>
-              </TextDiv>
-              <TextDiv>
-                <small>Detalhes: </small>
-                <strong>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Fugit, deserunt!
-                </strong>
-              </TextDiv>
-              <TextAreaDiv>
-                <small>Obs:</small>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Numquam laborum incidunt, beatae soluta fugit ratione. Sed et
-                  eius veritatis soluta, unde cupiditate nesciunt voluptas
-                  voluptatem. Molestiae optio nihil blanditiis quam?
-                </p>
-              </TextAreaDiv>
-            </CardBody>
-          </OrderBody>
-
-          {/* TODO Expand One and Compact two, vice versa */}
-          {/* Expanded height for all texts in expanded mode */}
-          <OrderHeader>Serviço</OrderHeader>
-          <OrderBody>
-            <CardBody>
-              <h5>Serviço</h5>
-              <DiagAreaDiv>
-                <span>
-                  Lorem ispanum dolor sit amet consectetur, adipisicing elit.
-                  Numquam laborum incidunt, beatae soluta fugit ratione. Sed et
-                  eius veritatis soluta, unde cupiditate nesciunt voluptas
-                  voluptatem. Molestiae optio nihil blanditiis quam?
-                </span>
-              </DiagAreaDiv>
-              <div>
-                <TextDiv>
-                  <small>Contatado dia: </small>
-                  <span>11/11/2011</span>
-                </TextDiv>
-                <TextDiv>
-                  <small>Prazo </small>
-                  <span>13/11/2011</span>
-                </TextDiv>
-              </div>
-              <div>
-                <button>aprovar</button>
-                <button>negar</button>
-                <TextDiv>
-                  <small>Total </small>
-                  <strong>1337,00</strong>
-                </TextDiv>
-              </div>
-            </CardBody>
-
-            <CardBody>
-              <h5>Reparo</h5>
-              <DiagAreaDiv>
-                <span>
-                  Lorem ispanum dolor sit amet consectetur, adipisicing elit.
-                  Numquam laborum incidunt, beatae soluta fugit ratione. Sed et
-                  eius veritatis soluta, unde cupiditate nesciunt voluptas
-                  voluptatem. Molestiae optio nihil blanditiis quam?
-                </span>
-              </DiagAreaDiv>
-              <div>
-                <TextDiv>
-                  <small>Efetuado: </small>
-                  <span>11/11/2011</span>
-                </TextDiv>
-                <TextDiv>
-                  <small>Total </small>
-                  <strong>1337,00</strong>
-                </TextDiv>
-              </div>
-            </CardBody>
-          </OrderBody>
-          {/* TODO Bottom card with images and payment */}
-        </>
-      ) : (
+      {loading ? (
         <>
           <OrderHeader>Order View</OrderHeader>
           <OrderBody>
-            <span>No Order Requested</span>
+            <span>Loading...</span>
           </OrderBody>
+        </>
+      ) : (
+        <>
+          {orderId && serviceOrder ? (
+            <>
+              {/* TODO Header Full grid block */}
+              <OrderHeader>Ordem de Serviço</OrderHeader>
+              <OrderBody>
+                <CardBody>
+                  <h5>Cliente</h5>
+                  <div>
+                    <TextDiv>
+                      <small>Nome: </small>
+                      <strong>{client && client.name}</strong>
+                    </TextDiv>
+                    <TextDiv>
+                      <small>Telefone 1: </small>
+                      <strong>{client && client.phone1}</strong>
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <TextDiv>
+                      <small>CPF: </small>
+                      <strong>{client && client.cpf}</strong>
+                    </TextDiv>
+                    <TextDiv>
+                      <small>Telefone 2: </small>
+                      <strong>{client && client.phone2}</strong>
+                    </TextDiv>
+                  </div>
+                  <TextDiv>
+                    <small>Email: </small>
+                    <strong>{client && client.email}</strong>
+                  </TextDiv>
+                  <TextDiv>
+                    <small>Endereço: </small>
+                    <strong>aaaaaaaaaaa</strong>
+                  </TextDiv>
+                  <TextAreaDiv>
+                    <small>Obs:</small>
+                    <p>{serviceOrder.observation}</p>
+                  </TextAreaDiv>
+                </CardBody>
+
+                <CardBody>
+                  <h5>Equipamento</h5>
+                  <div>
+                    <TextDiv>
+                      <small>Aparelho: </small>
+                      <strong>{equipment && equipment.name}</strong>
+                    </TextDiv>
+                    <TextDiv>
+                      <small>Modelo 1: </small>
+                      <strong>{serviceOrder.equipment_model}</strong>
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <TextDiv>
+                      <small>Serial: </small>
+                      <strong>{serviceOrder.serial_number}</strong>
+                    </TextDiv>
+                  </div>
+                  <TextDiv>
+                    <small>Acessórios: </small>
+                    <strong>{serviceOrder.accessories}</strong>
+                  </TextDiv>
+                  <TextDiv>
+                    <small>Detalhes: </small>
+                    <strong>{serviceOrder.details}</strong>
+                  </TextDiv>
+                  <TextAreaDiv>
+                    <small>Defeito</small>
+                    <p>{serviceOrder.problem_description}</p>
+                  </TextAreaDiv>
+                </CardBody>
+              </OrderBody>
+
+              {/* TODO Expand One and Compact two, vice versa */}
+              {/* Expanded height for all texts in expanded mode */}
+              <OrderHeader>Serviço</OrderHeader>
+              <OrderBody>
+                <CardBody>
+                  <h5>Serviço</h5>
+                  <DiagAreaDiv>
+                    <span>
+                      Lorem ispanum dolor sit amet consectetur, adipisicing
+                      elit. Numquam laborum incidunt, beatae soluta fugit
+                      ratione. Sed et eius veritatis soluta, unde cupiditate
+                      nesciunt voluptas voluptatem. Molestiae optio nihil
+                      blanditiis quam?
+                    </span>
+                  </DiagAreaDiv>
+                  <div>
+                    <TextDiv>
+                      <small>Contatado dia: </small>
+                      <span>11/11/2011</span>
+                    </TextDiv>
+                    <TextDiv>
+                      <small>Prazo </small>
+                      <span>13/11/2011</span>
+                    </TextDiv>
+                  </div>
+                  <div>
+                    <button>aprovar</button>
+                    <button>negar</button>
+                    <TextDiv>
+                      <small>Total </small>
+                      <strong>1337,00</strong>
+                    </TextDiv>
+                  </div>
+                </CardBody>
+
+                <CardBody>
+                  <h5>Reparo</h5>
+                  <DiagAreaDiv>
+                    <span>
+                      Lorem ispanum dolor sit amet consectetur, adipisicing
+                      elit. Numquam laborum incidunt, beatae soluta fugit
+                      ratione. Sed et eius veritatis soluta, unde cupiditate
+                      nesciunt voluptas voluptatem. Molestiae optio nihil
+                      blanditiis quam?
+                    </span>
+                  </DiagAreaDiv>
+                  <div>
+                    <TextDiv>
+                      <small>Efetuado: </small>
+                      <span>11/11/2011</span>
+                    </TextDiv>
+                    <TextDiv>
+                      <small>Total </small>
+                      <strong>1337,00</strong>
+                    </TextDiv>
+                  </div>
+                </CardBody>
+              </OrderBody>
+              {/* TODO Bottom card with images and payment */}
+            </>
+          ) : (
+            <>
+              <OrderHeader>Order View</OrderHeader>
+              <OrderBody>
+                <span>No Order Requested</span>
+              </OrderBody>
+            </>
+          )}
         </>
       )}
     </Container>
@@ -192,7 +209,7 @@ OrderView.defaultProps = {
 };
 
 const mapStateToProps = state => ({
-  serviceOrder: state.serviceOrder,
+  serviceOrder: state.serviceOrders.order,
 });
 
 const mapDispatchToProps = dispatch =>
